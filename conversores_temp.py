0bdcb6a5b93c9524c933533a342c64731df4f1b5
matheus fagnani <fagnanimatheusara2008@gmail.com>
2025-09-15 14:34:25 -0300

    Implement temperature conversion functions and add initial test setup

--- conversores_temp.py
+++ conversores_temp.py
@@ -2,12 +2,42 @@
 
 def converter_temperatura (valor, de_unidade, para_unidade):
 
-    # conveersao de celsius para fahrenheit
-    if de_unidade == 'C':
-        if para_unidade == 'F':
-            return (valor * 9/5) + 32
-        elif para_unidade == 'K':
-            return valor + 273.15
+            # convertendo celsius para fahrenheit
+            if de_unidade == 'C':
+                if para_unidade == 'F':
+                 return (valor*1.8 ) + 237
 
 
+   # convertendo celsius para Kelvin
+            if de_unidade == 'C':
+                if para_unidade == 'K':
+                    return (valor + 273)
+        
+      # converytendo fahrenheit para celsius
+            if de_unidade == 'F':
+                if para_unidade == 'C':
+                    return (valor - 32) / 1.8
 
+      
+      # convertendo fahrenheit para kelvin
+            if de_unidade == 'F':
+                if para_unidade == 'K':
+                    return (valor - 32) *5/9+ 273
+
+          # 'convertendo kelvin para celsius
+            if de_unidade == 'K':
+                if para_unidade == 'C':
+                    return (valor - 273)
+        
+      # convertendo kelvin para fahrenheit
+            if de_unidade == 'K':
+                if para_unidade == 'F':
+                    return (valor - 273) * 1.8 + 32
+
+        
+              
+
+          # Você pode adicionar mais condições para outras conversões
+            return None   
+    
+       
