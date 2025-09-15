a7141c63c939b27451da9142c46ef2c5d7cc3483
matheus fagnani <fagnanimatheusara2008@gmail.com>
2025-09-15 16:48:26 -0300

    .....

--- conversores_temp.py
+++ conversores_temp.py
@@ -8,28 +8,28 @@ def converter_temperatura (valor, de_escala, para_escala):
                  return (valor*1.8 ) + 237.15
 
 
-    # convertendo celsius para Kelvin
+        # convertendo celsius para Kelvin
             if de_escala == 'Celsius':
                 if para_escala == 'Kelvin':
                     return (valor + 273.15)
         
-        # converytendo fahrenheit para celsius
+            # converytendo fahrenheit para celsius
             if de_escala == 'Fahrenheit':
                 if para_escala == 'Celsius':
                     return (valor - 32) / 1.8
 
         
-        # convertendo fahrenheit para kelvin
+            #convertendo fahrenheit para kelvin
             if de_escala == 'Fahrenheit':
                 if para_escala == 'Kelvin':
                     return (valor - 32) *5/9+ 273.15
 
-            # 'convertendo kelvin para celsius
+            #convertendo kelvin para celsius
             if de_escala == 'Kelvin':
                 if para_escala == 'Celsius':
                     return (valor - 273.15)
         
-        # convertendo kelvin para fahrenheit
+            #convertendo kelvin para fahrenheit
             if de_escala == 'Kelvin':
                 if para_escala == 'Fahrenheit':
                     return (valor - 273.15) * 1.8 + 32
@@ -37,7 +37,7 @@ def converter_temperatura (valor, de_escala, para_escala):
         
                 
 
-            # Você pode adicionar mais condições para outras conversões
+            # bb ele vai retornar nada caso não colocar nada
             return None   
 
         
--- test/test_conversores_temp.py
+++ test/test_conversores_temp.py
@@ -1,8 +1,10 @@
 import pytest
 from conversores_temp import converter_temperatura
 
-def test_converter_temperatura():
-    # Teste de Celsius para Fahrenheit
+
+
+
+    
 
 
 
