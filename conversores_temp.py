cfb72a1396e40f01014332687287eee4f89b71f7
matheus fagnani <fagnanimatheusara2008@gmail.com>
2025-09-15 14:53:56 -0300

    Refactor temperature conversion function and improve test setup

--- conversores_temp.py
+++ conversores_temp.py
@@ -1,43 +1,43 @@
 # criando um conversor de temperaturas entre Celsius, Fahrenheit e Kelvin
 
-def converter_temperatura (valor, de_unidade, para_unidade):
+def converter_temperatura (valor, de_escala, para_escala):
 
             # convertendo celsius para fahrenheit
-            if de_unidade == 'C':
-                if para_unidade == 'F':
-                 return (valor*1.8 ) + 237
+            if de_escala == 'Celsius':
+                if para_escala == 'Fahrenheit':
+                 return (valor*1.8 ) + 237.15
 
 
-   # convertendo celsius para Kelvin
-            if de_unidade == 'C':
-                if para_unidade == 'K':
-                    return (valor + 273)
+    # convertendo celsius para Kelvin
+            if de_escala == 'Celsius':
+                if para_escala == 'Kelvin':
+                    return (valor + 273.15)
         
-      # converytendo fahrenheit para celsius
-            if de_unidade == 'F':
-                if para_unidade == 'C':
+        # converytendo fahrenheit para celsius
+            if de_escala == 'Fahrenheit':
+                if para_escala == 'Celsius':
                     return (valor - 32) / 1.8
 
-      
-      # convertendo fahrenheit para kelvin
-            if de_unidade == 'F':
-                if para_unidade == 'K':
-                    return (valor - 32) *5/9+ 273
-
-          # 'convertendo kelvin para celsius
-            if de_unidade == 'K':
-                if para_unidade == 'C':
-                    return (valor - 273)
         
-      # convertendo kelvin para fahrenheit
-            if de_unidade == 'K':
-                if para_unidade == 'F':
-                    return (valor - 273) * 1.8 + 32
+        # convertendo fahrenheit para kelvin
+            if de_escala == 'Fahrenheit':
+                if para_escala == 'Kelvin':
+                    return (valor - 32) *5/9+ 273.15
+
+            # 'convertendo kelvin para celsius
+            if de_escala == 'Kelvin':
+                if para_escala == 'Celsius':
+                    return (valor - 273.15)
+        
+        # convertendo kelvin para fahrenheit
+            if de_escala == 'Kelvin':
+                if para_escala == 'Fahrenheit':
+                    return (valor - 273.15) * 1.8 + 32
 
         
-              
+                
 
-          # Você pode adicionar mais condições para outras conversões
+            # Você pode adicionar mais condições para outras conversões
             return None   
-    
-       
+
+        
